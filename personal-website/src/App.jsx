ecddc8dec38bc2cf06d7c15503af6f26bfe0f08d
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-11-06 14:46:59 +0100

    Basic route , basic errorPage och main klara

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='min-h-screen flex flex-wrap content-between primary'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+      </div>
+    </div>
   )
 }
 
--- personal-website/src/main.jsx
+++ personal-website/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
+import Home from "./pages/Home.jsx"
+import AllProjects from "./pages/AllProjects.jsx"
+import ErrorPage from './pages/ErrorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    errorElement: <ErrorPage/>,
+    children:[
+      {path:"/", element: <Home/>},
+      {path:"/projects", element: <AllProjects/>},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
