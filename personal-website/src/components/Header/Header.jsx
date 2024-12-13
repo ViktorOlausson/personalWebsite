ce0855c4f66758dc64b63bfeb4c4709f4b891a8a
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-12-13 20:11:01 +0100

    samll tweaks to to menu

--- personal-website/src/components/Header/Header.jsx
+++ personal-website/src/components/Header/Header.jsx
@@ -3,10 +3,12 @@ import { useNavigate, useLocation } from "react-router-dom";
 import Container from '../Container/Container'
 import "../../index.css"
 import Button from '../Button'
-import { BeakerIcon, HomeIcon, BriefcaseIcon, InformationCircleIcon, EnvelopeIcon, ChevronLeftIcon } from '@heroicons/react/24/solid'
+//import { BeakerIcon, HomeIcon, BriefcaseIcon, InformationCircleIcon, EnvelopeIcon } from "@heroicons/react/24/solid"
+import { HomeIcon, BeakerIcon, BriefcaseIcon, InformationCircleIcon } from "@heroicons/react/solid"
+import {ChevronLeftIcon} from "@heroicons/react/solid"
 
 const iconSize = "size-6"
-const buttonStyle = "h-full flex items-center duration-200 hover:bg-button-color px-3"
+const buttonStyle = "h-full flex items-center duration-200 hover:bg-button-color px-3 bg-green-600"
 const buttonPadding = "px-3"
 const paragraphStyle = "mx-1.5 font-semibold"
 const liStyle = "mx-2"
@@ -23,9 +25,13 @@ const navItems = [
   {name: "Home", slug: "/", icon: <HomeIcon className={iconSize}/>},
   {name: "Projects", slug: "/projects", icon: <BeakerIcon className={iconSize}/>},
   {name: "Experience", slug: "/experience", icon: <BriefcaseIcon className={iconSize}/>, dropdownItems:[
-    {name: "Education", slug: "/"},{name: "Education", slug: "/"}, {name: "Education", slug: "/"}]},
+    {name: "Education", slug: "/"},{name: "Work", slug: "/"}, {name: "Knowledge", slug: "/"}]},
   {name: "About Me", slug: "/about-me", icon: <InformationCircleIcon className={iconSize}/>},
-  {name: "Contact", slug: "/contact", icon: <EnvelopeIcon className={iconSize}/>},
+  {name: "Contact", slug: "/contact", icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+  </svg>
+  },
 ]
 
 //{name: "Education", slug: "/"},{name: "Education", slug: "/"}, {name: "Education", slug: "/"}
@@ -36,24 +42,6 @@ function Header() {
   const currentPath = location.pathname
   const [hidden, setHidden] = useState(true)
   const [activeMenu, setActiveMenu] = useState(null)
-
-  // const _toggleMenu = (id) => {
-  //   setActiveMenu((prevActiveMenus) => ({
-  //    ...prevActiveMenus,
-  //     [id]:!prevActiveMenus[id],
-  //   }))
-  //   // if (id === activeMenu) {
-  //   //     setActiveMenu(null)
-  //   //   } else {
-  //   //     setActiveMenu(id)
-  //   //   }
-  // }
-
-  // const handelClickMenu = (id, slug) => {
-  //   setActiveMenu(id)
-  //   navigate(slug)
-  // }
-  //setHidden(false) 
   return (
     <header className='h-12 shadow-md shadow-box-shadow-gray navBackground place-content-center flex'>
       {/* border frame: border-2 border-border-gray */}
@@ -68,19 +56,20 @@ function Header() {
               <ul className='flex ml-auto'>
 
                 {navItems.map((item) => item.dropdownItems ? (
-                  <li className='mx-2' key={item.name}>
-                    <Button className={`${buttonStyle}  ${currentPath === item.slug ? "active" : ""}`} bgColor={buttonColor} padding={buttonPadding} 
+                  <li className='mx-1.5' key={item.name}>
+                    <Button className={`${buttonStyle}  ${currentPath === item.slug ? "active" : ""}`} bgColor={buttonColor} padding="px-0 pr-1 pl-3"
                     onMouseEnter={() => setHidden(false)} onMouseLeave={() => setHidden(true)} onClick={() => navigate(item.slug)} >
                       {item.icon}
                       <p className='mx-1.5 font-semibold'>{item.name}</p>
-                      <ChevronLeftIcon className={`${hidden ? arrowHidden : arrowShow} size-5 stroke-2 transition-all duration-300 `}/>
+                      <ChevronLeftIcon className={`${hidden ? arrowHidden : arrowShow} transition-all duration-300 size-8 px-0`} strokeWidth={2}/>
                     </Button>
                     <div className={`${hidden ? dropdownUnActive : dropdownActive}`} onMouseEnter={() => 
                     setHidden(false)} onMouseLeave={() => setHidden(true)}>
                       <ul className='flex flex-col py-2 justify-center'>
                         {item.dropdownItems.map((dropdownItem) => (
                           <li className='my-1' key={dropdownItem.name}>
-                            <Button className={`${buttonStyle} w-full h-8`} padding={buttonPadding} bgColor={buttonColor} onClick={() => navigate(dropdownItem.slug)}>
+                            <Button className={`${buttonStyle} w-full h-8`} padding={buttonPadding} bgColor={buttonColor}
+                            onClick={() => navigate(dropdownItem.slug)}>
                               <p className={paragraphStyle}>{dropdownItem.name}</p>
                             </Button>
                           </li>
@@ -89,7 +78,7 @@ function Header() {
                     </div>
                   </li>
                 ) : (
-                  <li className='mx-2' key={item.name}>
+                  <li className='mx-1.5' key={item.name}>
                     <Button className={`${buttonStyle}  ${currentPath === item.slug ? "active" : ""}`} bgColor={buttonColor} padding={buttonPadding} 
                     onClick={() => navigate(item.slug)}>
                       {item.icon}
