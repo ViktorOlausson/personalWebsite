8d02cffffed005e08dfd15170585ce86d5f0912c
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-12-14 18:01:07 +0100

    change to active color

--- personal-website/src/components/Header/Header.jsx
+++ personal-website/src/components/Header/Header.jsx
@@ -3,10 +3,10 @@ import { useNavigate, useLocation } from "react-router-dom";
 import Container from '../Container/Container'
 import "../../index.css"
 import Button from '../Button'
-//import { BeakerIcon, HomeIcon, BriefcaseIcon, InformationCircleIcon, EnvelopeIcon } from "@heroicons/react/24/solid"
 import { HomeIcon, BeakerIcon, BriefcaseIcon, InformationCircleIcon, AcademicCapIcon, OfficeBuildingIcon, CogIcon } from "@heroicons/react/solid"
 import {ChevronLeftIcon} from "@heroicons/react/solid"
 import Brain from '../icons/Brain';
+import Envolope from '../icons/Envolope';
 
 const iconSize = "size-6"
 const buttonStyle = "h-full flex items-center duration-200 hover:bg-button-color px-3"
@@ -30,14 +30,8 @@ const navItems = [
     {name: "Work", slug: "/experience/work", icon: <OfficeBuildingIcon className={iconSize}/>}, 
     {name: "Knowledge", slug: "/experience/knowledge", icon: <Brain/>}]},
   {name: "About Me", slug: "/about-me", icon: <InformationCircleIcon className={iconSize}/>},
-  {name: "Contact", slug: "/contact", icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-  </svg>
-  },
+  {name: "Contact", slug: "/contact", icon: <Envolope/>},
 ]
-
-//{name: "Education", slug: "/"},{name: "Education", slug: "/"}, {name: "Education", slug: "/"}<CogIcon className={iconSize}/>
 
 function Header() {
   const navigate = useNavigate()
@@ -51,16 +45,12 @@ function Header() {
       <Container className='place-content-center h-full ' maxWidth='max-w-9xl' paddingX=''>
         <nav className='flex h-full align-middle'>
           <div className='w-1/6 place-content-center'> {/* Logo div */}
-            {/* <Link to="/">
-              <Logo />
-            </Link> */}
           </div>
-          {/* Ändra färger på knappar?? */}
               <ul className='flex ml-auto'>
 
                 {navItems.map((item) => item.dropdownItems ? (
                   <li className='mx-1.5' key={item.name}>
-                    <Button className={`${buttonStyle}  ${currentPath === item.slug ? "active" : ""}`} bgColor={buttonColor} padding="px-0 pr-1 pl-3"
+                    <Button className={`${buttonStyle}  ${currentPath === item.slug ? "bg-button-color" : ""}`} bgColor={buttonColor} padding="px-0 pr-1 pl-3"
                     onMouseEnter={() => setHidden(false)} onMouseLeave={() => setHidden(true)} onClick={() => navigate(item.slug)} >
                       {item.icon}
                       <p className='mx-1.5 font-semibold'>{item.name}</p>
@@ -71,7 +61,7 @@ function Header() {
                       <ul className='flex flex-col py-2 justify-center'>
                         {item.dropdownItems.map((dropdownItem) => (
                           <li className='my-1.5' key={dropdownItem.name}>
-                            <Button className={`${buttonStyle} ${currentPath === dropdownItem.slug ? "active" : ""} w-full h-10`} padding={buttonPadding} bgColor={buttonColor}
+                            <Button className={`${buttonStyle} ${currentPath === dropdownItem.slug ? "bg-button-color" : ""} w-full h-10`} padding={buttonPadding} bgColor={buttonColor}
                             onClick={() => navigate(dropdownItem.slug)}>
                               {dropdownItem.icon}
                               <p className={paragraphStyle}>{dropdownItem.name}</p>
@@ -83,7 +73,7 @@ function Header() {
                   </li>
                 ) : (
                   <li className='mx-1.5' key={item.name}>
-                    <Button className={`${buttonStyle}  ${currentPath === item.slug ? "active" : ""}`} bgColor={buttonColor} padding={buttonPadding} 
+                    <Button className={`${buttonStyle}  ${currentPath === item.slug ? "bg-button-color" : ""}`} bgColor={buttonColor} padding={buttonPadding} 
                     onClick={() => navigate(item.slug)}>
                       {item.icon}
                       <p className='mx-1.5 font-semibold'>{item.name}</p>
