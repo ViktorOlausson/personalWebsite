54c66a5f8aad76df43bbb7fa48f3ee1376c2bdfa
ViktorOlausson <02viktorolausson@gmail.com>
2025-01-16 21:47:06 +0100

    Update WorkExperience.jsx

--- personal-website/src/pages/Experience/WorkExperience.jsx
+++ personal-website/src/pages/Experience/WorkExperience.jsx
@@ -5,7 +5,7 @@ function WorkExperience() {
   return (
     <div className='flex flex-row align-middle justify-center  w-full'>
       
-      <div className='flex flex-col sm:min-w-72 lg:w-1/4 max-w-xl shadow-md shadow-zinc-800'> {/* Container div */}
+      <div className='flex flex-col sm:min-w-72 lg:w-1/4 max-w-xl shadow-lg shadow-zinc-800'> {/* Container div */}
         <div className='bg-nav-prime px-2 py-0.5 h-14 flex flex-row align-middle justify-left'> {/* Heading div */}
           <div className='flex flex-col w-1/2'>
             <h1 className='text-white text-xl'><span className='font-semibold'>Företag AB, </span>plats - title</h1>
@@ -16,7 +16,7 @@ function WorkExperience() {
           </div>
           
         </div>
-        <div className='bg-dropdown w-full py-0.5 px-2 shadow-inner shadow-primary overflow-hidden transition-all duration-300 max-h-48'> {/* content */}
+        <div className={`bg-dropdown w-full py-0.5 px-2 shadow-inner shadow-primary overflow-hidden transition-all duration-300 max-h-48`}> {/* content */}
             description
         </div>
       </div>
