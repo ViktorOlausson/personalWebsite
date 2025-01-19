e160fdba243ae487ddc0ab25c4be63f4ee9fdaba
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-01-19 19:47:39 +0100

    Style and data maping done

--- personal-website/src/components/InfoCard.jsx
+++ personal-website/src/components/InfoCard.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 import {ChevronLeftIcon} from "@heroicons/react/solid"
 
 /// company used for school and business 
-//
+///
 
-function InfoCard(id, company = "", city = "", title = "", period= "", description= "") {
+function InfoCard({id, company = "", city = "", title = "", period= "", description= "", containerStyle=""}) {
   return (
-    <div className='flex flex-col min-w-96 lg:w-1/4 max-w-xl shadow-lg shadow-zinc-700'> {/* Container div */}
-        <div className='bg-nav-prime px-2 py-0.5 h-14 flex flex-row align-middle justify-left'> {/* Heading div */}
-          <div className='flex flex-col w-3/4 bg-orange-500'>
-            <h1 className='text-white text-xl'><span className='font-semibold'>{company}, </span>{city} - {title}</h1>
+    <div className={`flex flex-col min-w-96 lg:w-1/4 max-w-md shadow-lg shadow-zinc-700 ${containerStyle}`}> {/* Container div */}
+        <div className='bg-nav-prime px-1 py-0.5 h-14 flex flex-row align-middle justify-left'> {/* Heading div */}
+          <div className='flex flex-col w-5/6'>
+            <h1 className='text-white text-lg'><span className='font-semibold'>{company}, </span>{city} - {title}</h1>
             <h2>{period}</h2>
           </div>
-          <div className='w-1/4 flex justify-end items-center bg-red-700'>
+          <div className='w-1/6 flex justify-end items-center'>
             <ChevronLeftIcon className={`transition-all duration-300 size-12 px-0`} strokeWidth={2}/>
           </div>
           
         </div>
         <div className={`bg-dropdown w-full py-0.5 px-2 shadow-inner shadow-primary overflow-hidden transition-all duration-300 max-h-48`}> {/* content // dropdown container */}
-            {description}
+            <p className='text-white'>
+              {description}
+            </p>
         </div>
       </div>
   )
--- personal-website/src/pages/Experience/WorkExperience.jsx
+++ personal-website/src/pages/Experience/WorkExperience.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
-import Card from "../../components"
+import Card from "../../components/InfoCard"
 import workData from "../../Data/Work.json"
 
-const { company, location, title, time, description } = workData.Work; // göra detta till en array av object
+//const { company, location, title, time, description } = workData.Work; // göra detta till en array av object
+
 
 function WorkExperience() {
 
   const [active, setActive] = useState([])
 
   return (
-    <div className='flex flex-row align-middle justify-center  w-full'>
+    <div className='flex flex-row align-middle justify-center w-full'>
       
-      <Card/>
+      {/* <Card id="" company={company} city={location} title={title} period={time} description={description}/> */}
+
+      <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 items-center justify-between h-full'>
+      {workData.Work.map((work, index) => (
+        <Card key={index} company={work.company} city={work.location} title={work.title} period={work.time} description={work.description} containerStyle='mx-6 my-12'/>
+      ))}
+      </div>
 
     </div>
   )
