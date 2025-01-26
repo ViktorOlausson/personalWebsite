cd6f2ecfe508414627eed2bccd659bfea738b4da
ViktorOlausson <02viktorolausson@gmail.com>
2025-01-26 21:01:59 +0100

    JSON and style update

--- personal-website/src/components/InfoCard.jsx
+++ personal-website/src/components/InfoCard.jsx
@@ -9,8 +9,8 @@ function InfoCard({id, company = "", city = "", title = "", period= "", descript
     <div className={`flex flex-col min-w-96 lg:w-1/4 max-w-md shadow-lg shadow-zinc-700 ${containerStyle}`}> {/* Container div */}
         <div className='bg-nav-prime px-1 py-0.5 h-14 flex flex-row align-middle justify-left'> {/* Heading div */}
           <div className='flex flex-col w-5/6'>
-            <h1 className='text-white text-lg'><span className='font-semibold'>{company}, </span>{city} - {title}</h1>
-            <h2>{period}</h2>
+            <h1 className='text-white text-lg'><span className='font-semibold'>{company}, </span>{city}</h1>
+            <h2 className='text-neutral-300'>{title} - {period}</h2>
           </div>
           <div className='w-1/6 flex justify-end items-center'>
             <ChevronLeftIcon className={`transition-all duration-300 size-12 px-0`} strokeWidth={2}/>
